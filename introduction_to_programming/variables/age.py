227c783201f793df3e0c1abb2d59e779f810c6ea
Wonnie Ki <wonnie.tech@gmail.com>
2024-03-05 14:10:10 -0800

    Add code for problem #6 in Variables chapter

---
+++ introduction_to_programming/variables/age.py
@@ -0,0 +1,8 @@
+age = 20
+future_age = 10
+
+print(f'You are {age} years old.')
+print(f'In {future_age} years, you will be {age + 10} years old.')
+print(f'In {future_age * 2} years, you will be {age + 20} years old.')
+print(f'In {future_age * 3} years, you will be {age + 30} years old.')
+print(f'In {future_age * 4} years, you will be {age + 40} years old.')
