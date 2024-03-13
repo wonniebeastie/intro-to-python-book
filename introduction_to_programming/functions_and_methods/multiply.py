81b60b8279a1d9b8bec9e601d66bd362fe016bcc
Wonnie Ki <wonnie.tech@gmail.com>
2024-03-12 19:12:18 -0700

    Do exercise #3 of functions & methods chapter

---
+++ introduction_to_programming/functions_and_methods/multiply.py
@@ -0,0 +1,20 @@
+def multiply(first_number, second_number):
+    
+    result = first_number * second_number
+    print(f'{first_number} * {second_number} = {result}')
+
+first_number = int(input('Enter the first number: '))
+second_number = int(input('Enter the second number: '))
+
+
+multiply(first_number, second_number)
+
+
+
+
+
+
+# $ python multiply.py
+# Enter the first number: 3.1416
+# Enter the second number: 2.7183
+# 3.1416 * 2.7183 = 8.53981128
