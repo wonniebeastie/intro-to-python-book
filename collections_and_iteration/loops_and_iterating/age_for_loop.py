89e1f5e14ec3ff2c19e5559523cc7ef832a40fc6
Wonnie Ki <wonnie.tech@gmail.com>
2024-03-29 14:49:32 -0700

    Add exercises for loops & iterating chapter

---
+++ collections_and_iteration/loops_and_iterating/age_for_loop.py
@@ -0,0 +1,13 @@
+age = int(input('How old are you? '))
+print(f'You are {age} years old.')
+print()
+
+for year in range(10, 50, 10):
+    print(f'in {year} years, you will be {age + year} years old.')
+
+
+
+# print(f'In 10 years, you will be {age + 10} years old.')
+# print(f'In 20 years, you will be {age + 20} years old.')
+# print(f'In 30 years, you will be {age + 30} years old.')
+# print(f'In 40 years, you will be {age + 40} years old.')
